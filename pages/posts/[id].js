0058a2828859e220ea8601a940572a05b0a84f1a
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-22 23:46:42 +0900

    add: getStaticPathsを触った(ダイナミックルーティング)

---
+++ pages/posts/[id].js
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export default function Post({ post }) {
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Layout title={post.title}>
+      <p className="m-4">
+        {"ID:"}
+        {post.id}
+      </p>
+      <p className="mb-8 text-xl font-bold">{post.title}</p>
+      <p className="px-10">{post.body}</p>
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+            />
+          </svg>
+          <span>Back to blog-page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { post: post } = await getPostData(params.id);
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
